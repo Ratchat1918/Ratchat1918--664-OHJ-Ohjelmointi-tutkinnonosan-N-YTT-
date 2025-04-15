144a5a95602d406190b8b6179c7e3408492e777b
Henry Djupsjöbacka <henry.djupsjobacka@gmail.com>
2025-04-15 14:05:26 +0300

    rekisteröinti ja kirjautuminen valmiit

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-document.getElementById("loginBtn").addEventListener("click", function() {
+document.getElementById("logBtn").addEventListener("click", function() {
     document.getElementById("loginPanel").classList.add("open");
 });
 document.getElementById("closeBtn").addEventListener("click", function() {
@@ -15,20 +15,46 @@ document.getElementById("closeModal").addEventListener("click", function() {
 
 document.getElementById("registerBtn").addEventListener("click", function() {
     event.preventDefault();
-    checkPasswordUsername();
+    registerUser();
 });
 
-function checkPasswordUsername() {
-    console.log("tarkasta käyttis salasana");
+document.getElementById("loginBtn").addEventListener("click", function() {
+    event.preventDefault();
+    loginUser();
+})
+
+function registerUser() {
     let syötettyKäyttäjänimi = document.getElementById("newUsername").value;
     let syötettySalasana = document.getElementById("newPassword").value;
 
     if (syötettyKäyttäjänimi.length > 0 && syötettySalasana.length > 0) {
         alert("Tili luotu onnistuneesti.");
+        saveUser(syötettyKäyttäjänimi, syötettySalasana);
         document.getElementById("modal").style.display = "none";
         document.getElementById("newUsername").value = "";
         document.getElementById("newPassword").value = "";
     } else {
         alert("Käyttäjänimi tai salasana on virheellinen.");
+    }
+};
+
+function saveUser(username, password) {
+    let user = {name: username, password: password};
+    localStorage.setItem("userData", JSON.stringify(user));
+};
+
+function loginUser() {
+    let loginUsername = document.getElementById("username").value;
+    let loginPassword = document.getElementById("password").value;
+
+    let storedUserData = JSON.parse(localStorage.getItem("userData"));
+
+    if (storedUserData) {
+        if (loginUsername === storedUserData.name && loginPassword === storedUserData.password) {
+            document.getElementById("loggedUser").innerText = "Tervetuloa " + loginUsername + "!";
+            document.getElementById("loginPanel").classList.remove("open");
+        } else {
+            document.getElementById("inCorrect").innerText = "Virheellinen käyttäjänimi tai salasana."
+        }
     }
 };
